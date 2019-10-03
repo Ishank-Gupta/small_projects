b7837e8d90ab5e9f54be8a1bd39b3d5312a7dd0d
ISHANK KUMAR <35027525+Ishank-Gupta@users.noreply.github.com>
2019-10-03 12:02:00 +0530

    Update Tic_Tac_Toe.cpp

--- Tic_Tac_Toe.cpp
+++ Tic_Tac_Toe.cpp
@@ -1,46 +1,67 @@
 #include <iostream>
+#include <string>
 #include <set>
 
 using namespace std;
 
 char place[10] = { 'o', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
 set<int> num;
+string name1 = "", name2 = "";
 void board();
 int checkwin();
 
 
 int main()
 {
-	int player = 1, choice;
+	cout << "Player 1, Enter your name: ";
+	cin >> name1;
+	cout << "Player 2, Enter your name: ";
+	cin >> name2;
+	//
+	int player = 1, choice, game = 1;
 	int i = 1;
 	for (int i = 1; i < 10; ++i)
 		num.insert(i);
-	do
-	{
-		board();
-		player = (player % 2 != 0) ? 1 : 2;
-		cout << "Player " << player << ", Enter a number: ";
-		cin >> choice;
-		if ((choice > 0 && choice < 10) && (place[choice] != 'X' && place[choice] != 'O'))
+	do {
+		do
 		{
-			auto itr = num.find(choice);
-			if(itr != num.end())
-				num.erase(choice);
-			(player == 1) ? place[choice] = 'X' : place[choice] = 'O';
+			board();
+			player = (player % 2 != 0) ? 1 : 2;
+			if (player == 1)
+				cout << name1.c_str() << ", Enter a number: ";
+			else
+				cout << name2.c_str() << ", Enter a number: ";
+			cin >> choice;
+			if ((choice > 0 && choice < 10) && (place[choice] != 'X' && place[choice] != 'O'))
+			{
+				auto itr = num.find(choice);
+				if (itr != num.end())
+					num.erase(choice);
+				(player == 1) ? place[choice] = 'X' : place[choice] = 'O';
+			}
+			else {
+				cout << "Wrong choice.\n";
+				player--;
+			}
+			i = checkwin();
+			player++;
+		} while (i == -1);
+		board();
+		if (i == 1)
+		{
+			--player;
+			if (player == 1)
+				cout << "==>\a\t" << name1.c_str() << " win \n";
+			else
+				cout << "==>\a\t" << name2.c_str() << " win \n";
 		}
-		else {
-			cout << "Wrong choice.\n";
-			player--;
-		}
-		i = checkwin();
-		player++;
-	} while (i == -1);
-	board();
-	if (i == 1)
-		cout << "==>\a\tPlayer " << --player << " win \n";
-	else
-		cout << "==>\a\tGame draw\n";
-
+		else
+			cout << "==>\a\tGame draw\n";
+		system("pause");
+		system("cls");
+		cout << "Do you want to play again?\n1 --> Yes\n2 --> No\nEnter your choice: ";
+		cin >> game;
+	} while (game == 1);
 	system("pause");
 }
 
@@ -48,7 +69,7 @@ void board()
 {
 	system("cls");
 	cout << "\n\n\tTic Tac Toe\n\n";
-	cout << "Player 1 (X)  -  Player 2 (O)" << endl << endl;
+	cout << name1.c_str() << " (X)  -  " << name2.c_str() << " (O)" << endl << endl;
 	cout << endl;
 	cout << "     |     |     " << endl;
 	cout << "  " << place[1] << "  |  " << place[2] << "  |  " << place[3] << endl;
