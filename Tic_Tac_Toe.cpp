3d78645de906997f45d0307505924169b4f81505
ISHANK KUMAR <35027525+Ishank-Gupta@users.noreply.github.com>
2019-10-11 11:36:25 +0530

    Update Tic_Tac_Toe.cpp

--- Tic_Tac_Toe.cpp
+++ Tic_Tac_Toe.cpp
@@ -63,9 +63,16 @@ int main()
 			cout << "==>\a Game draw\n";
 		system("pause");
 		system("cls");
+again:
 		cout << "Do you want to play again?\n1 --> Yes\n2 --> No\nEnter your choice: ";
 		cin >> game;
-		if (game == 1)
+      if (game != 1 && game != 2)
+      {
+         system("cls");
+         std::cout << "It seems you have entered the wrong input. Try again!!!\n";
+         goto again;
+      }
+      if (game == 1)
 		{
 			for (int i = 1; i < 10; ++i)
 				place[i] = temp[i];
@@ -93,17 +100,18 @@ void board()
 
 int checkwin()
 {
-	if (num.empty())
-		return 0;
-	else if ((place[1] == place[2] && place[2] == place[3])
-			|| (place[4] == place[5] && place[5] == place[6])
-			|| (place[7] == place[8] && place[8] == place[9])
-			|| (place[1] == place[4] && place[4] == place[7])
-			|| (place[2] == place[5] && place[5] == place[8])
-			|| (place[3] == place[6] && place[6] == place[9])
-			|| (place[1] == place[5] && place[5] == place[9])
-			|| (place[3] == place[5] && place[5] == place[7]))
-		return 1;
-	else
-		return -1;
+
+   if ((place[1] == place[2] && place[2] == place[3])
+    || (place[4] == place[5] && place[5] == place[6])
+    || (place[7] == place[8] && place[8] == place[9])
+    || (place[1] == place[4] && place[4] == place[7])
+    || (place[2] == place[5] && place[5] == place[8])
+    || (place[3] == place[6] && place[6] == place[9])
+    || (place[1] == place[5] && place[5] == place[9])
+    || (place[3] == place[5] && place[5] == place[7]))
+      return 1;
+   else if (num.empty())
+      return 0;
+   else
+      return -1;
 }
